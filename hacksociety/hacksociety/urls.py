bd8542ffefb50da5600b62474cd09d5e7116d189
Bogdan Blidarescu <bogdan.blidarescu@gmail.com>
2016-12-09 17:51:22 +0200

    restrict better

--- hacksociety/hacksociety/urls.py
+++ hacksociety/hacksociety/urls.py
@@ -29,7 +29,7 @@ class EntrySerializer(serializers.HyperlinkedModelSerializer):
 
 # ViewSets define the view behavior.
 class EntryViewSet(viewsets.ModelViewSet):
-    queryset = Entry.objects.all()[:100]
+    queryset = Entry.objects.filter(id__lte=100)
     serializer_class = EntrySerializer
 
 # Routers provide an easy way of automatically determining the URL conf.
