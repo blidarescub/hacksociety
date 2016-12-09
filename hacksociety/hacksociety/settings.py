c00c181d4fa4c1fa14ae02820f37527bb2475b66
EC2 Default User <ec2-user@ip-172-31-10-246.eu-central-1.compute.internal>
2016-12-09 17:02:32 +0000

    migration 1

--- hacksociety/hacksociety/settings.py
+++ hacksociety/hacksociety/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'rest_framework'
+    'rest_framework',
+    'gov_api'
 ]
 
 MIDDLEWARE_CLASSES = [
