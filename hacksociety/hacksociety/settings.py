4b91374221005b648410d98e9ed4eae3ab71272e
Bogdan Blidarescu <bogdan.blidarescu@gmail.com>
2016-12-09 21:28:25 +0200

    static

--- hacksociety/hacksociety/settings.py
+++ hacksociety/hacksociety/settings.py
@@ -137,4 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '~/.virtualenvs/HACKSOCIETY/lib/python2.7/site-packages/django/contrib/admin/media',
+]
