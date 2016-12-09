f444b087a12db9e11a0b9801c7788828f91af7db
Bogdan Blidarescu <bogdan.blidarescu@gmail.com>
2016-12-09 19:41:10 +0200

    wefwegrt

--- hacksociety/hacksociety/settings.py
+++ hacksociety/hacksociety/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
 }
 
