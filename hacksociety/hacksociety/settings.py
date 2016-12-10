c2706375d4eefd2e3a4ad5244ad2fb5fbd723cb2
Bogdan Blidarescu <bogdan.blidarescu@gmail.com>
2016-12-10 11:22:53 +0200

    permissions

--- hacksociety/hacksociety/settings.py
+++ hacksociety/hacksociety/settings.py
@@ -115,6 +115,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100
 }
